e401cba692b4721d9708caa97e6b3fae709d1dd1
kulturman <arnaudbakyono@gmail.com>
2022-08-05 17:51:51 +0000

    Add solution to longestSubstringWithDistinctCharacters

---
+++ LongestSubstringWithDistinctCharacters.js
@@ -0,0 +1,24 @@
+function longestSubstringWithDistinctCharacters(str) {
+    let lastIndexOfCharacters = {};
+    let windowStart = 0;
+    let longestString = "";
+
+    for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
+        const currentCharacterLastIndex = lastIndexOfCharacters[str[windowEnd]];
+        //The character already exists in the map and is in the current window
+        if (currentCharacterLastIndex !== undefined && currentCharacterLastIndex >= windowStart) {
+            windowStart = currentCharacterLastIndex + 1;
+        }
+        lastIndexOfCharacters[str[windowEnd]] = windowEnd;
+
+        if (longestString.length < windowEnd - windowStart + 1) {
+            longestString = str.substring(windowStart, windowEnd + 1);
+        }
+    }
+
+    return longestString;
+}
+
+console.log(longestSubstringWithDistinctCharacters('aabccbb'));
+console.log(longestSubstringWithDistinctCharacters('abbbb'));
+console.log(longestSubstringWithDistinctCharacters('algorithms'));
