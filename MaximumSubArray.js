111d7e54739a082185580f4dea31f959071cba85
kulturman <arnaudbakyono@gmail.com>
2022-08-11 13:23:21 +0000

    Add solution to maximumm sub array problem

---
+++ MaximumSubArray.js
@@ -0,0 +1,22 @@
+function maximumSubArray(arr) {
+    let sum = 0;
+    let windowStart = 0;
+    let windowEnd = 0;
+    let maxSum = Number.MIN_SAFE_INTEGER;
+
+    for (windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+        sum += arr[windowEnd];
+
+        if (sum < 0) {
+            sum = 0;
+            windowStart++;
+        }
+
+        maxSum = Math.max(maxSum, sum);
+    }
+    return maxSum;
+}
+
+console.log(maximumSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
+console.log(maximumSubArray([-2, -1, -3, -4, -1, -2, 1, -5, 100]));
+console.log(maximumSubArray([100, -2, -1, -3, -4, -1, -2, 1, -5]));
