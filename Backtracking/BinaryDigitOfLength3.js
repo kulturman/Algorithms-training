41f79557853f287a5b324048b8042453f5c3a973
kulturman <arnaudbakyono@gmail.com>
2025-09-27 18:09:26 +0000

    Refacto

--- Backtracking/BinaryDigitOfLength3.js
+++ Backtracking/BinaryDigitOfLength3.js
@@ -12,17 +12,3 @@ function printCombinations(combination) {
 }
 
 printCombinations([]);
-
-function combinationsOfLetters() {
-    const letters = ['A', 'B', 'C'];
-
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            for (let k = 0; k < 3; k++) {
-                console.log(letters[i] + letters[j] + letters[k]);
-            }
-        }
-    }
-}
-
-//combinationsOfLetters()
